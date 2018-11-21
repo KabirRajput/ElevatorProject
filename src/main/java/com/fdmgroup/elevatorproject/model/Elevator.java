9e1736a031feaf4949a0283e48c97638f4547ff9
Denisse Sitoe <denisse.sitoe@fdmgroup.com>
2018-11-21 16:14:58 +0800

    Current version allows one elevator to move to different floors, change directions and restore to default values

--- src/main/java/com/fdmgroup/elevatorproject/model/Elevator.java
+++ src/main/java/com/fdmgroup/elevatorproject/model/Elevator.java
@@ -2,23 +2,41 @@ package com.fdmgroup.elevatorproject.model;
 
 import java.util.*;
 
-public class Elevator {
+public class Elevator implements Movable{
 
-	private int currentFloor;
-	private int minFloor = 0;
+	private int currentFloor = 0;
 	private ElevatorStatus currentStatus = ElevatorStatus.DEFAULT;
 	private ElevatorDirection dir = ElevatorDirection.UP;
-	private List<Integer> floorList = new LinkedList<Integer>();
-	
-	public void go(int i) {
-		setCurrentFloor(i);
+	private LinkedList<Integer> floorList = new LinkedList<Integer>();
+
+	public void go(int targetFloor) {
+
+		if(currentFloor - targetFloor > 0)
+		{
+			changeToDown();
+		}else {
+			changeToUp();
+		}
+
+		setCurrentFloor(targetFloor);
 	}
 
-	public void moveUp(){
+	public void elevatorListener() {
+		while(floorList.size() > 0) // only for Unit Test, change to while(true) for production
+		{
+			if(floorList.size() > 0) {
+				go(floorList.pop());
+			}
+			restoreToDefault();	
+		}
+
+	}
+
+	public void changeToUp(){
 		dir = ElevatorDirection.UP;
 	}
 
-	public void moveDown(){
+	public void changeToDown(){
 		dir = ElevatorDirection.DOWN;
 	}
 
@@ -37,13 +55,48 @@ public class Elevator {
 	public List<Integer> getFloorList() {
 		return floorList;
 	}
-	
+
 	public void addFloor(int floor) {
 		floorList.add(floor);
 	}
-	
+
 	public void addFloor(int floorIndex, int floor) {
 		floorList.add(floorIndex, floor);
 	}
 
+	public ElevatorStatus getElevatorStatus() {
+		return currentStatus;
+	}
+
+	public ElevatorDirection getDirection() {
+		return dir;
+	}
+
+	public void accelerate(int cost) {
+		// TODO Auto-generated method stub
+		currentStatus = ElevatorStatus.ACCELARATING;
+	}
+
+	public void decelerate(int cost) {
+		// TODO Auto-generated method stub
+		currentStatus = ElevatorStatus.DECELERATING;
+	}
+
+	public void changeFloor(int cost) {
+		// TODO Auto-generated method stub
+		currentStatus = ElevatorStatus.CHANGINGFLOORS;
+	}
+
+	public void serviceFloor(int cost) {
+		// TODO Auto-generated method stub
+		currentStatus = ElevatorStatus.SERVICING;
+	}
+
+	public void restoreToDefault() {
+		// TODO Auto-generated method stub
+		go(0);
+		dir = ElevatorDirection.UP;
+		currentStatus = ElevatorStatus.DEFAULT;
+
+	}
 }
